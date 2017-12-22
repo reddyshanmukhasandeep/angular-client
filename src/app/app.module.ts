82ebf86890218d03d77af5c8f4afbfbc4ae68f1d
sandeep <reddyshanmukha.sandeep@cognizant.com>
2017-12-22 13:44:15 +0530

    angular-code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Routes,RouterModule} from '@angular/router';
+import  {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { AddProjectComponent } from './add-project/add-project.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
 
+
+const ROUTES: Routes =[
+{path:'AddProject', component:AddProjectComponent},
+{path:'AddTask', component:AddTaskComponent},
+{path:'AddUser', component:AddUserComponent},
+{path:'ViewTask',component:ViewTaskComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent,
+    AddProjectComponent,
+    AddTaskComponent,
+    ViewTaskComponent
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
